902b7102b3f7a5b7282385e036b2b45b7564da55
Maja <maja.miljak@zivver.com>
2024-06-22 18:29:08 +0200

    Add models

---
+++ Models/AddressFormatNetherlands.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AddressValidator.Models
+{
+    public class AddressFormatNetherlands
+    {
+        [JsonPropertyName("Country")]
+        public required string CountryCode { get; set; }
+        public string? City { get; set; }
+        public string? Street { get; set; }
+        public required string Zipcode { get; set; }
+        public int HouseNumber { get; set; }
+    }
+}
---
+++ Models/AddressFormatUSA.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AddressValidator.Models
+{
+    public class AddressFormatUSA
+    {
+        [JsonPropertyName("Country")]
+        public required string CountryCode { get; set; }
+        public string? StreetName { get; set; }
+        public string? StreetNumber { get; set; }
+        public string? Apartment { get; set; }
+        public string? City { get; set; }
+        public string? ZipCode { get; set; }
+    }
+}
